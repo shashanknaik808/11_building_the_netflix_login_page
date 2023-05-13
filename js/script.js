083ff296058a13eb07d4c1cc5e3e87136d592a90
Shashank Naik <shashanknaik808@gmail.com>
2023-05-13 22:10:33 +0530

    Finishing the Form Validation

--- js/script.js
+++ js/script.js
@@ -21,18 +21,34 @@ function validateInput(e) {
         classes = states[0];
     }
 
-    //   console.log(classes);
+    console.log(classes);
+
+    // remove the previous class
+    e.target.nextElementSibling.classList.remove(...states);
+
+    // add the new class
+    e.target.nextElementSibling.classList.add(classes);
 
 
     // generate or remove the alert
     if (classes === 'not-valid') {
-        const errorDiv = document.createElement('div');
-        errorDiv.appendChild(document.createTextNode('This field is mandatory'));
-        errorDiv.classList.add('alert');
+        // in the case that the alert doesn't exists, add an alert
+        if (e.target.parentElement.nextElementSibling.classList[0] !== 'alert') {
+            const errorDiv = document.createElement('div');
+            errorDiv.appendChild(document.createTextNode('This field is mandatory'));
+            errorDiv.classList.add('alert');
 
-        console.log(errorDiv);
+            console.log(errorDiv);
 
-        // inject the error inside the dom, before the next field
-        e.target.parentElement.parentElement.insertBefore(errorDiv, e.target.parentElement.nextElementSibling);
+            // inject the error inside the dom, before the next field
+            e.target.parentElement.parentElement.insertBefore(errorDiv, e.target.parentElement.nextElementSibling);
+        }
+    }
+    else {
+        // the input has something
+        if (e.target.parentElement.nextElementSibling.classList[0] === 'alert') {
+            // only run when the alert exists
+            e.target.parentElement.nextElementSibling.remove();
+        }
     }
 }
